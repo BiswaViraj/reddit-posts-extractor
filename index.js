4dc676518ed5c9d93360cbbac3cf4cf0df3aa5d6
BiswaViraj <biswajeetdas18@gmail.com>
2020-03-18 14:18:06 +0530

    DONE: Fetch 20 latest posts from subreddits

---
+++ index.js
@@ -0,0 +1,28 @@
+const snoowrap = require("snoowrap");
+require("dotenv").config();
+// API set-up, replace with your credentials
+const r = new snoowrap({
+    userAgent: process.env.USER_AGENT,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    username: process.env.USER_NAME,
+    password: process.env.PASSWORD
+});
+// enter your favourite subreddits here
+let subreddits = ["cscareerquestions", "learnprogramming"];
+let newPosts = [];
+
+// maps over each subreedits
+// and fetches top 20 new posts
+subreddits.map(subreddit => {
+    r.getSubreddit(subreddit)
+        .getNew({ limit: 20 })
+        .map(post => {
+            newPosts.push({ title: post.title, url: post.url });
+        })
+        .then(() => {
+            console.log(subreddit);
+            console.log(newPosts);
+            newPosts = [];
+        });
+});
